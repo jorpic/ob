83778eaf87cf5d87ac972e3aefd4c57a5951e01a
Max Taldykin <jorpic@gmail.com>
2014-12-06 22:41:10 +0300

    Simple WS mockup

--- src/Main.hs
+++ src/Main.hs
@@ -1,33 +1,46 @@
 
 module Main where
 
+import Control.Monad
 import Control.Monad.IO.Class
+import Control.Concurrent (forkIO)
+
+import Data.Text (Text)
+import qualified Data.Text.Encoding as T
+import qualified Data.Text.IO as T
 
 import Data.Monoid
+import Data.Aeson ((.=))
 import qualified Data.Aeson as Aeson
+import Data.Aeson.TH
 
 import System.Environment (getArgs)
+import System.Posix (createDirectory)
 import qualified Data.Configurator as Config
 import Data.Configurator.Types (Config)
 
+import Data.UUID as UUID
+import Data.UUID.V4 as UUID
 import Web.Scotty
 import Network.Wai.Middleware.Static (staticPolicy, noDots, addBase)
+
+import qualified Network.WebSockets as WS
 
 
 
 main :: IO ()
 main = getArgs >>= \case
   [configFile] -> do
-    Config.load [Config.Required configFile]
-      >>= startEngines
+    conf <- Config.load [Config.Required configFile]
+    void $ forkIO $ wsServer conf
+    httpServer conf
   _ -> error "Usage: ob <config>"
 
 
-startEngines :: Config -> IO ()
-startEngines conf = do
+httpServer :: Config -> IO ()
+httpServer conf = do
   Just httpPort  <- Config.lookup conf "http.port"
---  Just wsoskPort <- Config.lookup conf "websock.port"
---  Just graphPort <- Config.lookup conf "graphite.port"
+  Just wsockPort <- Config.lookup conf "websocket.port"
 
   scotty httpPort $ do
     middleware $ staticPolicy (noDots <> addBase "static")
@@ -36,12 +49,38 @@ startEngines conf = do
       setHeader "Content-Type" "text/html"
       file "static/index.html"
 
-    post "/test" $ do
-      x <- jsonData
-      -- get url
-      -- gen uuid
-      -- create tmp dir
-      -- create tank.ini
-      -- spawn thread: run tank >> report end of job
-      -- return uuid
-      json (x :: Aeson.Value)
+    post "/bang" $ do
+      BangQuery{url} <- jsonData -- TODO: check url => ssl, port, url
+      uuid <- liftIO UUID.nextRandom
+      json $ Aeson.object
+        ["job" .= UUID.toString uuid
+        ,"ws_port" .= (wsockPort :: Int)
+        ]
+
+
+wsServer :: Config -> IO ()
+wsServer conf = do
+  Just wsPort <- Config.lookup conf "websocket.port"
+  WS.runServer "0.0.0.0" wsPort $ \req -> do
+    let url = T.decodeUtf8 $ WS.requestPath $ WS.pendingRequest req
+    T.putStrLn url
+    -- check if == uuid
+    conn <- WS.acceptRequest req
+    -- send allData
+    WS.sendTextData conn ("hello" :: Text)
+    -- loop sendNewData `finally` disconnect
+    WS.sendClose conn ("bye" :: Text) -- FIXME: wait for close from peer
+
+
+
+
+runTank :: Config -> UUID -> Text -> IO ()
+runTank conf uuid url = do
+--  Just graphitePort <- Config.lookup conf "graphite.port"
+  Just tankDir <- Config.lookup conf "tank.data_dir"
+  createDirectory (tankDir ++ "/" ++ UUID.toString uuid) 0o777
+  -- create tank.ini
+  -- spawn thread: run tank >> report end of job
+
+data BangQuery = BangQuery { url :: Text }
+$(deriveFromJSON defaultOptions ''BangQuery)
--- static/coffee/main.coffee
+++ static/coffee/main.coffee
@@ -1,1 +1,10 @@
-# nothing here yet
+
+orangeClick = ->
+  inp = document.getElementById 'url'
+  req = JSON.stringify(url: inp.value)
+  d3.json('/bang').post req, (err, rsp) ->
+    wsURL = "ws://#{location.hostname}:#{rsp.ws_port}/#{rsp.job}"
+    ws = new WebSocket wsURL
+    ws.onopen = ->
+    ws.onmessage = (m) -> console.debug m
+    ws.onclose = ->
